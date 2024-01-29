21707b3a2f873f59f0e8269c3d59f59c17faff48
FarhanAmir306 <amirf3051@gmail.com>
2024-01-30 01:51:45 +0600

    update

--- Fashion_Cloth/settings.py
+++ Fashion_Cloth/settings.py
@@ -31,6 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS=True
 CSRF_TRUSTED_ORIGINS = ['https://fashion-cloth.onrender.com']
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:8000',

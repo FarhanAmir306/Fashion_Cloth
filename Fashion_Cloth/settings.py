104053a30acdb008ca4721fde55782c928c89c3e
FarhanAmir306 <amirf3051@gmail.com>
2024-01-30 01:13:05 +0600

    update

--- Fashion_Cloth/settings.py
+++ Fashion_Cloth/settings.py
@@ -29,9 +29,9 @@ SECRET_KEY =env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://fashion-cloth.onrender.com','https://*.127.0.0.1','fashion-cloth.onrender.com','http://localhost:5500']
+ALLOWED_HOSTS = ['https://fashion-cloth.onrender.com','https://*.127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ['https://fashion-cloth.onrender.com','https://*.127.0.0.1','fashion-cloth.onrender.com','http://localhost:5500']
+CSRF_TRUSTED_ORIGINS = ['https://fashion-cloth.onrender.com','https://*.127.0.0.1']
 
 # Application definition
 

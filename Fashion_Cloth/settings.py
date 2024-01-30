98076d4b2a023f040c9c0db6c72f3ba2bfd4168e
FarhanAmir306 <amirf3051@gmail.com>
2024-01-30 18:39:30 +0600

    last

--- Fashion_Cloth/settings.py
+++ Fashion_Cloth/settings.py
@@ -34,14 +34,17 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_ALL_ORIGINS=True
 
 CSRF_TRUSTED_ORIGINS = ['https://fashion-cloth.onrender.com']
+
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:8000',
     'http://127.0.0.1:5500',
+    'https://fashion-cloth.onrender.com',
 )
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     'http://127.0.0.1:5500',
+    'https://fashion-cloth.onrender.com',
 ]
 CORS_ALLOW_METHODS = (
     "DELETE",

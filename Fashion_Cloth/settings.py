6633d17cd58cd24f381d01a9b4ebcf6c1a578e6b
FarhanAmir306 <amirf3051@gmail.com>
2024-01-30 23:00:20 +0600

    last

--- Fashion_Cloth/settings.py
+++ Fashion_Cloth/settings.py
@@ -31,29 +31,25 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_ALL_ORIGINS=True
+# CORS_ALLOW_ALL_ORIGINS=True
 
 CSRF_TRUSTED_ORIGINS = ['https://fashion-cloth.onrender.com']
 
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:8000',
-    'http://127.0.0.1:5500',
-    'https://fashion-cloth.onrender.com',
-)
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    'http://127.0.0.1:5500',
-    'https://fashion-cloth.onrender.com',
-]
-CORS_ALLOW_METHODS = (
-    "DELETE",
-    "GET",
-    "OPTIONS",
-    "PATCH",
-    "POST",
-    "PUT",
-)
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:8080",
+#     'http://127.0.0.1:5500',
+#     'https://fashion-cloth.onrender.com',
+# ]
+# CORS_ALLOW_METHODS = (
+#     "DELETE",
+#     "GET",
+#     "OPTIONS",
+#     "PATCH",
+#     "POST",
+#     "PUT",
+# )
 
 # Application definition
 

f8c15009a489794a20a6bfbf45980073a80a74ae
FarhanAmir306 <amirf3051@gmail.com>
2024-01-31 20:37:32 +0600

    last

--- products/models.py
+++ products/models.py
@@ -38,7 +38,7 @@ from django.core.validators import MinValueValidator
 class BestSeller(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity_sold = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0)])
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True,null=True)
 
     def __str__(self):
         return f"{self.product.name} - {self.quantity_sold} Sold"
